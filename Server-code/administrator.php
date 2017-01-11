4da6d51eee476b5da465b0017d439db9cb5e0aad
XuXinting <xuxinting1995523@qq.com>
2017-01-11 20:31:34 +0800

    change init info

--- Server-code/administrator.php
+++ Server-code/administrator.php
@@ -1,11 +1,79 @@
 <?php
 /*
-进行项目管理，接口版
+进行项目管理，网页版
 
 只有限定的初始管理员可以进行用户的操作；
 用户可以对自己的传感器进行操作
 */
 
+/*
+POST
+	UPASS	用户密码
+	UID		用户ID
+	UNAME	用户名
+*/
 
+header('Content-type: text/html; charset=UTF-8');
+
+require_once("config.php");
+require_once("functions_db.php");
+
+$UPASS = $_POST['UPASS'];
+$UID = $_POST['UID'];
+$UNAME = $_POST['UNAME'];
+
+$loged = true;
+if(strlen((string)$UID) != 9){
+	$loged = false;
+
+	if(!$loged){
+		
+	}else{
+		$log_checked = false;
+		$table_name = NAME_OF_TABLE_USER;
+		$ROWS = array("user_pass");
+		$CONSTRAIN = "$table_name.user_id='$UID' AND $table_name.user_name='$UNAME'";
+		
+		$RESULT = db_select($table_name,$ROWS,$CONSTRAIN);
+		
+		if(strcmp((string)$RESULT[0]['user_pass'],(string)$UPASS) == 0){
+			$table_name = NAME_OF_TABLE_SENSOR;
+			$ROWS = array("sensor_id","sensor_name","sensor_type","sensor_value","sensor_status","sensor_capture");
+			$CONSTRAIN = "$table_name.user_id='$UID'";
+			
+			$RESULT = db_select($table_name,$ROWS,$CONSTRAIN);
+			
+			echo "<table width='80%' border=1 align='center' cellpadding=5 cellspacing=0>";
+			echo '<tr align="center"><td>编号</td><td>名称</td><td>值</td></tr>';
+			
+			foreach($RESULT as $K=>$V){
+				echo '<tr align="center">';
+				echo "<td>$V['sensor_id']</td><td>$V['sensor_name']</td>";
+				
+				if($V['sensor_type'] == '1'){
+					echo "<td>$V['sensor_value']</td>";
+				}
+				if($V['sensor_type'] == '2'){
+					if($V['sensor_status'] == '1'){
+						echo "<td>开</td>";
+					}else{
+						echo "<td>关</td>";
+					}
+				}
+				if($V['sensor_type'] == '3'){
+					$img = base64_decode($V['sensor_capture']);
+					header('Content-type: image/jpg');
+					echo "<br>$content<br>";
+				}
+				echo '</tr>';
+			}
+			
+			echo '</table>';
+		}else{
+			$UID = "";
+			$domain_name = DOMAIN_NAME;
+			header("Location:http://$domain_name/administrator.php"); 
+		}
+	}
 
 ?>
--- Server-code/init.php
+++ Server-code/init.php
@@ -41,11 +41,60 @@ $INIT_CAPTURE_TABLE = "INSERT INTO $TABLE_CAPTURE (capture_num,sensor_id,capture
 $INIT_SENSOR_LOG = "INSERT INTO $TABLE_LOG (log_num,sensor_id,sensor_value,log_datetime) 
 				VALUES ('1','200000000','0','$ROOT_USER_REGDATE');";
 
-//初始化清空表
-$CLEAR_TABLE = "DELETE FROM $TABLE_USER;
-				DELETE FROM $TABLE_SENSOR;
-				DELETE FROM $TABLE_CAPTURE;
-				DELETE FROM $TABLE_LOG;";
+//删除所有表
+$DELETE_TABLE = "DROP TABLE $TABLE_CAPTURE;DROP TABLE $TABLE_LOG;DROP TABLE $TABLE_SENSOR;DROP TABLE $TABLE_USER;";
+
+				
+//创建表
+$CREATE_TABLE_USER = "CREATE TABLE IF NOT EXISTS `user_info` (
+  `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户的ID标识，自增，从100000000开始',
+  `user_name` varchar(50) NOT NULL COMMENT '用户名',
+  `user_pass` varchar(255) NOT NULL COMMENT '加密后的密文',
+  `user_regdate` datetime NOT NULL COMMENT '用户的注册时间，用于和用户名生成盐',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  KEY `user_pass` (`user_pass`)
+) AUTO_INCREMENT=100000000 DEFAULT CHARSET=utf8 COMMENT='用户信息存储表，用于身份验证';";
+
+$CREATE_TABLE_SENSOR = "CREATE TABLE IF NOT EXISTS `sensor_info` (
+  `sensor_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '传感器编号，自增，从200000000开始',
+  `user_id` int(11) UNSIGNED NOT NULL COMMENT '传感器所属的用户ID',
+  `sensor_name` varchar(40) DEFAULT NULL COMMENT '传感器的名称',
+  `sensor_type` int(4) UNSIGNED NOT NULL DEFAULT '0' COMMENT '传感器类型，1表示数值型，2表示开关型，3表示图像型',
+  `last_modified` datetime NOT NULL COMMENT '上次修改时间',
+  `sensor_value` int(5) NOT NULL DEFAULT '0' COMMENT '传感器数值，默认0，只针对1-数值型',
+  `sensor_status` int(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '传感器状态，默认0，只针对2-开关型，1表示开',
+  `sensor_capture` longtext COMMENT '传感器捕获的图片文件，默认为空',
+  PRIMARY KEY (`sensor_id`),
+  UNIQUE KEY `sensor_id` (`sensor_id`),
+  KEY `user_id` (`user_id`)
+)AUTO_INCREMENT=200000005 DEFAULT CHARSET=utf8 COMMENT='传感器信息定义表';";
+
+$CREATE_TABLE_LOG = "CREATE TABLE IF NOT EXISTS `sensor_log` (
+  `log_num` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '顺序编号，从1开始',
+  `sensor_id` int(11) UNSIGNED NOT NULL COMMENT '传感器编号，不为空，外键',
+  `sensor_value` int(1) UNSIGNED NOT NULL COMMENT '传感器数值，数值型与开关型统一记录',
+  `log_datetime` datetime NOT NULL COMMENT '传感器数据上传日期时间',
+  PRIMARY KEY (`log_num`),
+  KEY `sensor_id` (`sensor_id`)
+)AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COMMENT='传感器数据历史记录表';";
+
+$CREATE_TABLE_CAPTURE = "CREATE TABLE IF NOT EXISTS `capture_info` (
+  `capture_num` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '捕获的顺序编号，从1开始自增编号',
+  `sensor_id` int(11) UNSIGNED NOT NULL COMMENT '传感器的编号，和sensor_info表对应',
+  `capture_datetime` datetime NOT NULL COMMENT '图片的捕获时间',
+  `sensor_capture` longtext NOT NULL COMMENT '传感器捕获的图片文件(BASE64编码)，默认非空',
+  PRIMARY KEY (`capture_num`),
+  UNIQUE KEY `capture_num` (`capture_num`),
+  KEY `sensor_id` (`sensor_id`)
+)AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;";
+
+$CREATE_TABLE_KEYS = "ALTER TABLE `capture_info`
+  ADD CONSTRAINT `sensor_constrain` FOREIGN KEY (`sensor_id`) REFERENCES `sensor_info` (`sensor_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ALTER TABLE `sensor_info`
+  ADD CONSTRAINT `user_constrains` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ALTER TABLE `sensor_log`
+  ADD CONSTRAINT `sensor_id` FOREIGN KEY (`sensor_id`) REFERENCES `sensor_info` (`sensor_id`) ON DELETE CASCADE ON UPDATE CASCADE";
 
 //建立连接
 $conn = mysqli_connect(DATA_HOST_M,DATA_HOST_USER,DATA_HOST_PASS,DATA_HOST_DBNAME);
@@ -61,7 +110,12 @@ if(!$conn){
 		echo "Connection established.";echo "<br>";
 	
 	//判断清空结果
-	$result = mysqli_query($conn,$CLEAR_TABLE);
+	$result = mysqli_query($conn,$DELETE_TABLE);
+	$result = mysqli_query($conn,$CREATE_TABLE_USER);
+	$result = mysqli_query($conn,$CREATE_TABLE_SENSOR);
+	$result = mysqli_query($conn,$CREATE_TABLE_LOG);
+	$result = mysqli_query($conn,$CREATE_TABLE_CAPTURE);
+	$result = mysqli_query($conn,$CREATE_TABLE_KEYS);
 	
 	if(DEBUG_MODE){
 		echo '$CLEAR_TABLE = '; echo $CLEAR_TABLE; echo "<br>";
